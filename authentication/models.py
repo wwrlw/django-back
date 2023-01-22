e951af95263417722e0fbfba33bbb4a56b396851
Tima <timas3811@gmail.com>
2023-01-23 01:17:58 +0300

    auth model and migr

--- authentication/models.py
+++ authentication/models.py
@@ -1,5 +1,7 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+
+from authentication.managers import UserManager
 
 # Create your models here.
 
@@ -7,7 +9,7 @@ class User(AbstractBaseUser):
     email = models.EmailField(verbose_name='Email addres' ,max_length=255, unique=True)
     first_name = models.CharField(verbose_name='Имя', max_length=255)
     second_name = models.CharField(verbose_name='Фамилия', max_length=255)
-    photo = models.ImageField(verbose_name='Фото', upload_at='users/photos')
+    photo = models.ImageField(verbose_name='Фото', upload_to='users/photos')
     bio = models.TextField(verbose_name='Биография', max_length=255 )
 
 
@@ -16,8 +18,10 @@ class User(AbstractBaseUser):
     is_superuser = models.BooleanField(verbose_name='', default=False)
 
 
-USERNAME_FIELD = 'email'
-REQUIRED_FIELDS: list['first_name', 'second_name',]
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'second_name', ]
+ 
+objects = UserManager()
 
 def __str__ (self):
     return self.email
--- first_project/settings.py
+++ first_project/settings.py
@@ -71,6 +71,8 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'first_project.wsgi.application'
 
+AUTH_USER_MODEL = 'authentication.User'
+
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
