15276c41ef447e844ddbb9c78b1f5a6f5b3d9940
Tima <timas3811@gmail.com>
2023-01-24 21:18:27 +0300

    api start

--- artist/views.py
+++ artist/views.py
@@ -1,5 +1,8 @@
-from django.http import HttpResponse
+from django.http import JsonResponse
+from artist.models import Artist
 
 def artist_list(request):
-    return HttpResponse()
+    artists = Artist.objects.all()
+    print(artists)
+    return JsonResponse(data={'status':'ok'})
     
